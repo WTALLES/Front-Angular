03db4c1e39098f61a1e39147e63b019ac8daf77d
Cassio Tavares das Silva <cassio.tavares4@gmail.com>
2024-07-30 15:29:14 -0400

    atualizando a tela de login e cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,15 @@ import {TelaConsulDocsComponent} from "./tela-consul-docs/tela-consul-docs.compo
 import {TelaChamadoTecnicoComponent} from "./tela-chamado-tecnico/tela-chamado-tecnico.component";
 import { RecuperaSenhaComponent } from './recupera-senha/recupera-senha.component';
 
+
 const routes: Routes = [
   {path: 'login', component:TelaLoginComponent},
   {path:'t1',component:TelaEscolhaAudDocsComponent},
   {path:'t2', component: TelaFormOcorrenciaComponent},
   {path:'t3', component:TelaConsulDocsComponent},
   {path:'t4',component:TelaChamadoTecnicoComponent},
-  {path: 'rec', component:RecuperaSenhaComponent}
+  { path: 'recupera-senha', component: RecuperaSenhaComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 
 ];
 
--- src/app/tela-login/tela-login.component.ts
+++ src/app/tela-login/tela-login.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tela-login',
   templateUrl: './tela-login.component.html',
   styleUrls: ['./tela-login.component.css']
 })
-export class TelaLoginComponent {
-  username: string = '';
-  password: string = '';
+export class TelaLoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      usuario: ['', Validators.required],
+      senha: ['', Validators.required]
+    });
+  }
 
   onSubmit() {
-    // Lógica de autenticação
-    console.log('Usuário:', this.username);
-    console.log('Senha:', this.password);
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      // Adicione a lógica de login aqui
+    }
+  }
+
+  onRecuperarSenha() {
+    this.router.navigate(['/recupera-senha']);
   }
 }
