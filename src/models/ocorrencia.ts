553dce3a07410c83247098cda92d8d1af9a6ff6b
Wtalles <cassio.tavares4@gmail.com>
2024-08-22 10:22:28 -0400

    models ocorrencia

---
+++ src/models/ocorrencia.ts
@@ -0,0 +1,14 @@
+export class Ocorrencia{
+  n_maquina: string;
+  cliente: string;
+  operador1: string;
+  id: string ;
+  operador:string;
+  status: string;
+  ugb: string;
+  tpOcorrencia: string;
+  tamanhoLote: string;
+  turno: string;
+  re: string;
+  cod_producto: string;
+}
