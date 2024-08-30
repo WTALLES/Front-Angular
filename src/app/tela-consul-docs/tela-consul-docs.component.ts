fedf0eca6f979022b1ebaec67d8f36c5bc6ce55e
Nicolas Pimentel <129703371+nicaodever@users.noreply.github.com>
2024-08-30 08:38:28 -0400

    filtro

--- src/app/tela-consul-docs/tela-consul-docs.component.ts
+++ src/app/tela-consul-docs/tela-consul-docs.component.ts
@@ -1,11 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {Produto} from "../../models/produto";
 import {AppService} from "../app.service";
 import {Ocorrencia} from "../../models/ocorrencia";
 import {AngularFireStorage} from "@angular/fire/compat/storage";
-import {finalize} from "rxjs";
+import {finalize, map} from "rxjs";
 import {onChildChanged} from "@angular/fire/database";
 import {MenuLateralService} from "../menu-lateral.service";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Funcionario} from "../../models/funcionario";
+import {Router} from "@angular/router";
+import {FloatLabelType} from "@angular/material/form-field";
+import {toSignal} from "@angular/core/rxjs-interop";
 
 
 @Component({
@@ -17,13 +22,14 @@ export class TelaConsulDocsComponent implements OnInit{
   produtos: Produto[] = [];
   produto: Produto = new Produto();
   ocorrencias: Ocorrencia[] = [];
-  constructor(private service: AppService, private menuLateralSerivce: MenuLateralService) { }
+  constructor(private service: AppService, private menuLateralSerivce: MenuLateralService,private fb: FormBuilder) { }
 
   ngOnInit() {
    this.getApi();
     this.menuLateralSerivce.sidebarVisibility$.subscribe((isVisible) => {
       this.isSidebarVisible = isVisible;
     });
+    this.valiLogin()
   }
 
   getApi(){
@@ -41,6 +47,35 @@ export class TelaConsulDocsComponent implements OnInit{
   //respansividade em relação ao menu
   isSidebarVisible = true;
 
+  //
+  searchForm: FormGroup;
+  funcionarios: Funcionario[] = []
+  re: string
+  ocorrencia: Ocorrencia = new Ocorrencia()
+  idselecionado: string
+  valid: boolean = false
+  valiLogin(){
+    this.searchForm = this.fb.group({
+      tpOcorrencia: ['', Validators.required],
+    });
+  }
 
+  compararListaComBanco(): void {
+    for (let item of this.ocorrencias) {
+      if (item.tpOcorrencia === this.ocorrencia.tpOcorrencia) {
+        alert(this.ocorrencia.tpOcorrencia)
+        this.idselecionado = this.ocorrencia.tpOcorrencia;
+        this.valid = true
+      } else {
+        console.log('Nenhum match encontrado');
+      }
+    }
+  }
+
+  readonly floatLabelControl = new FormControl('auto' as FloatLabelType);
+  protected readonly floatLabel = toSignal(
+    this.floatLabelControl.valueChanges.pipe(map(v => v || 'auto')),
+    {initialValue: 'auto'},
+  );
 
 }
